ebee59754c882e85fb26656db7811a515699af81
qwerty2501 <qwerty2501@live.jp>
2018-07-23 01:15:45 +0900

    modified prefix.

--- src/engine/wasm.rs
+++ src/engine/wasm.rs
@@ -4,10 +4,10 @@ use parity_wasm::elements::Module as ParityWasmModule;
 use super::llvm::*;
 use failure::Error;
 use std::str;
-pub const WASM_CALL_PREFIX:&str = "WASM_CALL_";
+pub const WASM_FUNCTION_PREFIX:&str = "WASM_FUNCTION_";
 
 pub fn to_wasm_call_name(name:&str)->String{
-    [WASM_CALL_PREFIX,name].concat()
+    [WASM_FUNCTION_PREFIX,name].concat()
 }
 
 
