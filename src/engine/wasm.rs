f41d5c40dfffbd1bd4a78e706638aafe527c208a
qwerty2501 <qwerty2501@live.jp>
2018-07-31 01:52:54 +0900

    split initialize instructions.

--- src/engine/wasm.rs
+++ src/engine/wasm.rs
@@ -1,6 +1,6 @@
 
 
-use parity_wasm::elements::Module as ParityWasmModule;
+use parity_wasm::elements::Module as WasmModule;
 use super::*;
 use failure::Error;
 use std::str;
@@ -23,7 +23,7 @@ impl<T:WasmIntType> WasmCompiler<T>{
     }
 
 
-    pub fn compile<'c>(&self, module_id:&str,wasm_module:&ParityWasmModule,context:&'c Context)->Result<ModuleGuard<'c>,Error> {
+    pub fn compile<'c>(&self, module_id:&str,wasm_module:&WasmModule,context:&'c Context)->Result<ModuleGuard<'c>,Error> {
         let build_context = BuildContext::new(module_id,context);
         self.build_init_data_sections_function(wasm_module,&build_context)?;
         Ok(build_context.move_module())
@@ -34,42 +34,42 @@ impl<T:WasmIntType> WasmCompiler<T>{
         build_context.module().set_function("init_data_sections",Type::function(Type::int8(build_context.context()),&param_types,true))
     }
 
-    fn build_init_global_sections(&self,wasm_module:&ParityWasmModule,module:&Module,builder:&Builder)->Result<(),Error>{
+    fn build_init_global_sections(&self,wasm_module:&WasmModule,build_context:&BuildContext)->Result<(),Error>{
         let import_global_count = wasm_module.import_section().map_or(0,|section|{
            section.entries().iter().filter(|entry|match entry.external() {
                External::Global(_)=>true,
                _=>false,
-           }).count()
+           }).count() as u32
         });
         wasm_module.global_section().map_or(Ok(()),|section|{
 
             for (index,entry) in section.entries().iter().enumerate(){
-                self.set_global(module,index + import_global_count,entry.global_type());
+                self.build_const_initialize_global(build_context, index as u32 + import_global_count, entry);
             }
             Ok(())
         })
     }
 
-    fn build_global_entry<'a>(& self,module:&'a Module,builder:&Builder,index:usize,entry:&GlobalEntry)->Result<(),Error>{
-        let global_value = self.set_global(module,index,&entry.global_type());
-        let context = module.context();
-        let instruction = entry.init_expr().code().first().ok_or(NotExistGlobalInitializerInstruction)?;
-        let initialize_value = self.build_instruction(module,builder,instruction)?.ok_or(NotExistGlobalInitializerInstruction)?;
-        global_value.set_initializer(initialize_value);
-        global_value.set_global_const( entry.global_type().is_mutable());
-        Ok(())
+    fn build_const_initialize_global<'a>(&self, build_context:&'a BuildContext, index:u32, global_entry:&GlobalEntry)->Result<&'a Value,Error>{
+        let g = self.declare_global(build_context,index,global_entry.global_type());
+        let instruction = global_entry.init_expr().code().first().ok_or(NotExistGlobalInitializerInstruction)?;
+        match instruction{
+            Instruction::I32Const(v) => Some(instructions::i32_const(build_context,*v)),
+            Instruction::I64Const(v)=>Some(instructions::i64_const(build_context,*v)),
+            Instruction::F32Const(v) => Some(instructions::f32_const(build_context,f32_reinterpret_i32(*v as i32))),
+            Instruction::F64Const(v)=>Some(instructions::f64_const(build_context,f64_reinterpret_i64(*v as i64))),
+            _=>None,
+
+        }.map(|const_initializer|{
+            g.set_initializer(const_initializer);
+            g.set_global_const(global_entry.global_type().is_mutable());
+        });
+        Ok(g)
     }
 
-    fn get_global<'a>(& self,module:&'a Module,index:usize)->Result< &'a Value,Error>{
-        let name = Self::get_global_name(index);
-        Ok(module.get_named_global(name.as_ref()).ok_or_else(|| NoSuchLLVMGlobalValue {name})?)
-    }
 
-    fn get_global_name(index:usize) -> String {
-        [WASM_GLOBAL_PREFIX,index.to_string().as_ref()].concat()
-    }
-    fn set_global<'a>(& self,module:&'a Module,index:usize,global_type:&GlobalType)->&'a Value{
-        module.set_global(Self::get_global_name(index).as_ref(),Self::value_type_to_type(&global_type.content_type(),module.context()))
+    fn declare_global<'a>(& self, build_context:&'a BuildContext, index:u32, global_type:&GlobalType) ->&'a Value{
+        build_context.module().set_global(instructions::get_global_name(index).as_ref(),Self::value_type_to_type(&global_type.content_type(),build_context.context()))
     }
 
     fn value_type_to_type<'a>(value_type:&'a ValueType,context:&'a Context)->&'a Type{
@@ -81,19 +81,7 @@ impl<T:WasmIntType> WasmCompiler<T>{
         }
     }
 
-    fn build_instruction<'a>(&self,module:&'a Module,builder:&'a Builder,instruction:&Instruction)->Result<Option<&'a Value>,Error>{
-        let context = module.context();
-        match instruction{
-            Instruction::I64Const(v) => Ok(Some(Value::const_int(Type::int64(context),*v as u64,true))),
-            Instruction::I32Const(v) => Ok(Some(Value::const_int(Type::int32(context),*v as u64,true))),
-            Instruction::F32Const(v) => Ok(Some(Value::const_real(Type::float32(context),f32_reinterpret_i32(*v as i32) as ::libc::c_double))),
-            Instruction::F64Const(v) => Ok(Some(Value::const_real(Type::float64(context),f64_reinterpret_i64(*v as i64) as ::libc::c_double))),
-            Instruction::GetGlobal(v) =>self.get_global(module,*v as usize).map(|r|Some(r)),
-            instruction => Err(InvalidInstruction {instruction:instruction.clone()})?,
-        }
-    }
-
-    fn build_init_data_sections_function(&self,wasm_module:&ParityWasmModule,build_context:&BuildContext)->Result<(),Error>{
+    fn build_init_data_sections_function(&self,wasm_module:&WasmModule,build_context:&BuildContext)->Result<(),Error>{
         let function = self.set_init_data_sections_function(build_context);
         build_context.builder().build_function(build_context.context(),function,|builder,bb|{
             wasm_module.data_section().map_or(Ok(()),|data_section|{
@@ -111,7 +99,12 @@ impl<T:WasmIntType> WasmCompiler<T>{
             Value::const_int(Type::int_wasm_ptr::<T>(build_context.context()),v,true)
         };
         let instruction = segment.offset().code().first().ok_or(NotExistDataSectionOffset)?;
-        let offset = self.build_instruction(build_context.module(),build_context.builder(),instruction)?.ok_or(NotExistDataSectionOffset)?;
+        let offset = match instruction {
+            Instruction::I64Const(v)=>Ok(instructions::i64_const(build_context,*v)),
+            Instruction::I32Const(v)=>Ok(instructions::i32_const(build_context,*v )),
+            Instruction::GetGlobal(v)=>instructions::get_global(build_context,*v ),
+            invalid_instruction => Err(InvalidInstruction {instruction:invalid_instruction.clone()})?,
+        }?;
         let dest = self.linear_memory_compiler.build_get_real_address(build_context,offset,"dest",segment.index() as usize);
         let c_args = segment.value().iter().map(|v|Value::const_int(Type::int8(build_context.context()),*v as ::libc::c_ulonglong,false)).collect::<Vec<&Value>>();
         let src = Value::const_vector( &c_args);
@@ -160,7 +153,6 @@ mod tests{
         let context = Context::new();
 
         let build_context = BuildContext::new("build_data_segment_works",&context);
-
     }
 
 
