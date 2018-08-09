10b3138360b2529ae1bc010f13212dbd428e8671
qwerty2501 <qwerty2501@live.jp>
2018-08-09 21:35:24 +0900

    order arguments.

--- src/engine/wasm.rs
+++ src/engine/wasm.rs
@@ -26,8 +26,8 @@ impl<T:WasmIntType> WasmCompiler<T>{
 
     pub fn compile<'c>(&self, module_id:&str,wasm_module:&WasmModule,context:&'c Context)->Result<ModuleGuard<'c>,Error> {
         let build_context = BuildContext::new(module_id,context);
-        self.build_init_global_sections(wasm_module,&build_context)?;
-        self.build_init_data_sections_function(wasm_module,&build_context)?;
+        self.build_init_global_sections(&build_context,wasm_module)?;
+        self.build_init_data_sections_function(&build_context,wasm_module)?;
         Ok(build_context.move_module())
     }
 
@@ -40,22 +40,22 @@ impl<T:WasmIntType> WasmCompiler<T>{
         build_context.module().set_declare_function("init_data_sections", Type::function(Type::int8(build_context.context()), & [], false))
     }
 
-    fn build_init_global_sections(&self,wasm_module:&WasmModule, build_context:&BuildContext)->Result<(),Error>{
+    fn build_init_global_sections(&self,build_context:&BuildContext,wasm_module:&WasmModule )->Result<(),Error>{
         let import_global_count = wasm_module.import_count(ImportCountType::Global) as u32;
         wasm_module.global_section().map_or(Ok(()),|section|{
-            self.build_global_entries(section.entries(),import_global_count,build_context)
+            self.build_global_entries(build_context,section.entries(),import_global_count)
         })
     }
 
-    fn build_global_entries(&self,entries:&[GlobalEntry],import_global_count:u32,build_context:&BuildContext)->Result<(),Error>{
+    fn build_global_entries(&self,build_context:&BuildContext,entries:&[GlobalEntry],import_global_count:u32)->Result<(),Error>{
         for (index,entry) in entries.iter().enumerate(){
-            self.build_const_initialize_global( index as u32 + import_global_count, entry,build_context)?;
+            self.build_const_initialize_global( build_context,index as u32 + import_global_count, entry)?;
         }
         Ok(())
     }
 
-    fn build_const_initialize_global<'a>(&self, index:u32, global_entry:&GlobalEntry,build_context:&'a BuildContext)->Result<&'a Value,Error>{
-        let g = self.set_declare_global(index, global_entry.global_type(), build_context);
+    fn build_const_initialize_global<'a>(&self, build_context:&'a BuildContext,index:u32, global_entry:&GlobalEntry)->Result<&'a Value,Error>{
+        let g = self.set_declare_global(build_context,index, global_entry.global_type());
         let instruction = global_entry.init_expr().code().first().ok_or(NotExistGlobalInitializerInstruction)?;
         match instruction{
             Instruction::I32Const(v) => Some(instructions::i32_const(build_context,*v)),
@@ -72,7 +72,7 @@ impl<T:WasmIntType> WasmCompiler<T>{
     }
 
 
-    fn set_declare_global<'a>(& self, index:u32, global_type:&GlobalType, build_context:&'a BuildContext) ->&'a Value{
+    fn set_declare_global<'a>(& self, build_context:&'a BuildContext, index:u32, global_type:&GlobalType) ->&'a Value{
         build_context.module().set_declare_global(instructions::get_global_name(index).as_ref(), Self::value_type_to_type(&global_type.content_type(), build_context.context()))
     }
 
@@ -85,19 +85,19 @@ impl<T:WasmIntType> WasmCompiler<T>{
         }
     }
 
-    fn build_init_data_sections_function(&self,wasm_module:&WasmModule,build_context:&BuildContext)->Result<(),Error>{
+    fn build_init_data_sections_function(&self,build_context:&BuildContext,wasm_module:&WasmModule)->Result<(),Error>{
         let function = self.set_declare_init_data_sections_function(build_context);
         build_context.builder().build_function(build_context.context(),function,|builder,bb|{
             wasm_module.data_section().map_or(Ok(()),|data_section|{
                for segment in data_section.entries(){
-                    self.build_data_segment(segment,build_context)?;
+                    self.build_data_segment(build_context,segment)?;
                }
                 Ok(())
             })
         })
     }
 
-    fn build_data_segment(&self,segment:&DataSegment,build_context:&BuildContext)->Result<(),Error>{
+    fn build_data_segment(&self,build_context:&BuildContext,segment:&DataSegment)->Result<(),Error>{
         let instruction = segment.offset().code().first().ok_or(NotExistDataSectionOffset)?;
         let offset = match instruction {
             Instruction::I64Const(v)=>Ok(instructions::i64_const(build_context,*v)),
@@ -160,11 +160,11 @@ mod tests{
         let context = Context::new();
         let build_context = BuildContext::new("build_global_entries_works",&context);
         let compiler = WasmCompiler::<u32>::new();
-        compiler.build_global_entries( &[
+        compiler.build_global_entries( &build_context,&[
             GlobalEntry::new(GlobalType::new(ValueType::I32,false),InitExpr::new(vec![
                 Instruction::I32Const(33),
             ]))
-        ],0,&build_context)?;
+        ],0,)?;
         test_initializer(get_global(&build_context,0)?,33,true,|initializer|initializer.const_int_get_sign_extended_value());
         Ok(())
 
@@ -188,9 +188,9 @@ mod tests{
         let context = Context::new();
         let build_context = BuildContext::new("build_const_initialize_global_works_i32",&context);
         let compiler = WasmCompiler::<u32>::new();
-        compiler.build_const_initialize_global(0,&GlobalEntry::new(GlobalType::new(ValueType::I32,false),InitExpr::new(vec![
+        compiler.build_const_initialize_global(&build_context,0,&GlobalEntry::new(GlobalType::new(ValueType::I32,false),InitExpr::new(vec![
             Instruction::I32Const(22),
-        ])),&&build_context)?;
+        ])),)?;
         test_initializer(get_global(&build_context,0)?,22,true,|initializer|initializer.const_int_get_sign_extended_value());
         Ok(())
     }
@@ -200,9 +200,9 @@ mod tests{
         let context = Context::new();
         let build_context = BuildContext::new("build_const_initialize_global_works_i64",&context);
         let compiler = WasmCompiler::<u32>::new();
-        compiler.build_const_initialize_global(0,&GlobalEntry::new(GlobalType::new(ValueType::I64,true),InitExpr::new(vec![
+        compiler.build_const_initialize_global(&build_context,0,&GlobalEntry::new(GlobalType::new(ValueType::I64,true),InitExpr::new(vec![
             Instruction::I64Const(5667),
-        ])),&&build_context)?;
+        ])),)?;
         test_initializer(get_global(&build_context,0)?,5667,false,|initializer|initializer.const_int_get_sign_extended_value());
         Ok(())
     }
@@ -212,9 +212,9 @@ mod tests{
         let context = Context::new();
         let build_context = BuildContext::new("build_const_initialize_global_works_f32",&context);
         let compiler = WasmCompiler::<u32>::new();
-        compiler.build_const_initialize_global(0,&GlobalEntry::new(GlobalType::new(ValueType::F32,false),InitExpr::new(vec![
+        compiler.build_const_initialize_global(&build_context,0,&GlobalEntry::new(GlobalType::new(ValueType::F32,false),InitExpr::new(vec![
             Instruction::F32Const(i32_reinterpret_f32(4.00) as u32),
-        ])),&&build_context)?;
+        ])),)?;
 
         test_initializer(get_global(&build_context,0)?,4.00,true,|initializer|{
             let mut loses_info = false;
@@ -229,9 +229,9 @@ mod tests{
         let context = Context::new();
         let build_context = BuildContext::new("build_const_initialize_global_works_f64",&context);
         let compiler = WasmCompiler::<u32>::new();
-        compiler.build_const_initialize_global(0,&GlobalEntry::new(GlobalType::new(ValueType::F64,true),InitExpr::new(vec![
+        compiler.build_const_initialize_global(&build_context,0,&GlobalEntry::new(GlobalType::new(ValueType::F64,true),InitExpr::new(vec![
             Instruction::F64Const(i64_reinterpret_f64(4.00) as u64),
-        ])),&&build_context)?;
+        ])),)?;
 
         test_initializer(get_global(&build_context,0)?,4.00,false,|initializer|{
             let mut loses_info = false;
@@ -259,7 +259,7 @@ mod tests{
 
         let function_name = "build_data_segment_works";
         build_test_function(&build_context,function_name,&[],|builder,bb|{
-            compiler.build_data_segment(&data_segment,&build_context)?;
+            compiler.build_data_segment(&build_context,&data_segment,)?;
             build_context.builder().build_ret_void();
             Ok(())
         })?;
