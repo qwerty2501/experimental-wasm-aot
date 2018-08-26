90bed1eaa6dfe9aa88954b98b7869f9b9b345155
qwerty2501 <qwerty2501@live.jp>
2018-08-27 02:37:57 +0900

    fix update wasm file bug

--- build.rs
+++ build.rs
@@ -8,15 +8,12 @@ use std::path::Path;
 use std::ffi::OsString;
 fn main(){
 
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let manifest_dir = Path::new(&manifest_dir);
 
-    let out_dir = env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".to_string());
+    let out_dir = env::var("CARGO_TARGET_DIR").map(|v|Path::new(&v).to_path_buf()).unwrap_or_else(|_| manifest_dir.join("target"));
     let dir = "test_cases";
-    build_wat(Path::new(dir), Path::new(&out_dir).join(dir).as_path());
-
-    Command::new("wat2wasm").args(&["test_cases/wasm_compiler/return_only/return_only.wat","-o"])
-        .arg(&format!("{}/return_only.wasm",out_dir))
-        .status()
-        .unwrap();
+    build_wat(manifest_dir.join(dir).as_path(), out_dir.join(dir).as_path());
 
 }
 
@@ -32,12 +29,15 @@ fn build_wat(dir:&Path,out_dir:&Path){
         let ext = entry_path.extension().map_or_else(||"".to_string(),|ext|ext.to_str().unwrap_or("").to_string());
         let stem = entry_path.file_stem().map_or_else(||"".to_string(),|v|v.to_str().unwrap_or("").to_string());
 
+
         let out_path = out_dir.join(&stem);
+        let out_wasm_path = out_path.with_extension("wasm");
         let path_in_project = dir.join(file_name);
 
-        if   meta_data.is_file() && ext == "wat"  &&  (!out_path.exists() || filetime::FileTime::from_last_access_time(&meta_data) >= filetime::FileTime::from_last_access_time(&out_path.metadata().unwrap())) {
+        if   meta_data.is_file() && ext == "wat"  &&  (!out_wasm_path.exists() || filetime::FileTime::from_last_modification_time(&meta_data) >= filetime::FileTime::from_last_modification_time(&out_wasm_path.metadata().unwrap())) {
+
             Command::new("wat2wasm").args(&[path_in_project.to_str().unwrap(),"-o"])
-                .arg(&format!("{}.wasm",out_path.to_str().unwrap()))
+                .arg(&out_wasm_path)
                 .status()
                 .unwrap();
         } else if meta_data.is_dir(){
