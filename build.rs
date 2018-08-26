420973cd7ba11779d6f1f458c7401fdb1b396a58
qwerty2501 <qwerty2501@live.jp>
2018-08-27 01:39:24 +0900

    modified arguments types.

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn main(){
 
     let out_dir = env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".to_string());
     let dir = "test_cases";
-    build_wat(dir,&format!("{}/{}",out_dir,dir));
+    build_wat(Path::new(dir), Path::new(&out_dir).join(dir).as_path());
 
     Command::new("wat2wasm").args(&["test_cases/wasm_compiler/return_only/return_only.wat","-o"])
         .arg(&format!("{}/return_only.wasm",out_dir))
@@ -21,7 +21,7 @@ fn main(){
 }
 
 
-fn build_wat(dir:&str,out_dir:&str){
+fn build_wat(dir:&Path,out_dir:&Path){
     fs::create_dir_all(out_dir);
     for entry in fs::read_dir(dir).unwrap(){
         let entry = entry.unwrap();
@@ -32,19 +32,16 @@ fn build_wat(dir:&str,out_dir:&str){
         let ext = entry_path.extension().map_or_else(||"".to_string(),|ext|ext.to_str().unwrap_or("").to_string());
         let stem = entry_path.file_stem().map_or_else(||"".to_string(),|v|v.to_str().unwrap_or("").to_string());
 
-        let out_path = OsString::from( format!("{}/{}",out_dir,stem ));
-        let out_path = Path::new(  &out_path) ;
-        let path_in_project = OsString::from(format!("{}/{}",dir,file_name));
-        let path_in_project = Path::new(&path_in_project);
-        let path_in_project_str = path_in_project.to_str().unwrap_or("");
+        let out_path = out_dir.join(&stem);
+        let path_in_project = dir.join(file_name);
 
         if   meta_data.is_file() && ext == "wat"  &&  (!out_path.exists() || filetime::FileTime::from_last_access_time(&meta_data) >= filetime::FileTime::from_last_access_time(&out_path.metadata().unwrap())) {
-            Command::new("wat2wasm").args(&[path_in_project_str,"-o"])
+            Command::new("wat2wasm").args(&[path_in_project.to_str().unwrap(),"-o"])
                 .arg(&format!("{}.wasm",out_path.to_str().unwrap()))
                 .status()
                 .unwrap();
         } else if meta_data.is_dir(){
-            build_wat(path_in_project_str,out_path.to_str().unwrap());
+            build_wat(path_in_project.as_path(),out_path.as_path());
         }
 
     }
