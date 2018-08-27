fa440c60554ebe9ee9fda54774b3cfc80cbdfe55
qwerty2501 <qwerty2501@live.jp>
2018-08-27 23:03:18 +0900

    to be test functions.

--- src/engine/test_utils.rs
+++ src/engine/test_utils.rs
@@ -7,6 +7,7 @@ use std::path::Path;
 use std::env;
 use std::path::PathBuf;
 
+#[cfg(test)]
 pub fn get_target_dir()->Result<PathBuf,Error>{
    env::var("CARGO_TARGET_DIR").map(|v|Ok(Path::new(&v).to_path_buf())).unwrap_or_else(|_| {
         let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
@@ -15,15 +16,18 @@ pub fn get_target_dir()->Result<PathBuf,Error>{
     })
 }
 
+#[cfg(test)]
 pub fn build_test_function<F:FnOnce(& Builder,& BasicBlock) -> Result<(),Error>>(build_context:&BuildContext,function_name:&str,args:&[&Value],on_build:F)->Result<(),Error>{
     build_test_function_with_return(build_context,function_name,Type::void(build_context.context()),args,on_build)
 }
 
+#[cfg(test)]
 pub fn build_test_function_with_return<F:FnOnce(& Builder,& BasicBlock) -> Result<(),Error>>(build_context:&BuildContext,function_name:&str, return_type:&Type, args:&[&Value],on_build:F)->Result<(),Error>{
     let function = build_context.module().set_declare_function(function_name,Type::function(return_type,&args.iter().map(|v|Type::type_of(v)).collect::<Vec<_>>(),false));
     build_context.builder().build_function(build_context.context(),function,on_build)
 }
 
+#[cfg(test)]
 fn init_test_jit() ->Result<(),Error>{
     link_in_mc_jit();
     initialize_native_target()?;
@@ -31,6 +35,7 @@ fn init_test_jit() ->Result<(),Error>{
     Ok(())
 }
 
+#[cfg(test)]
 pub fn test_module_in_engine<F:FnOnce(&ExecutionEngine)->Result<(),Error>>(module:&Module,f:F)->Result<(),Error>{
     init_test_jit()?;
     let engine = ExecutionEngine::new_for_module(module)?;
@@ -38,6 +43,8 @@ pub fn test_module_in_engine<F:FnOnce(&ExecutionEngine)->Result<(),Error>>(modul
     engine.remove_module(module)?;
     Ok(())
 }
+
+#[cfg(test)]
 pub fn run_test_function_with_name<'a>(engine:&ExecutionEngine, module:&'a Module, function_name:&str, args:&[&GenericValue]) ->Result<GenericValueGuard<'a>,Error>{
     let function = module.get_named_function(function_name).ok_or_else(||NoSuchLLVMFunction{ name:function_name.to_string()})?;
     Ok(engine.run_function(function,args))
